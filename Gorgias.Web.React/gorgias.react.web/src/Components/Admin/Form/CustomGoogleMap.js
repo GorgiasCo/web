8321fc6eb6280c69554fb0ba3dcb4cd67e0d6133
nimamyscreen <zhosto@yahoo.com>
2018-02-20 02:38:44 +0800

    google map lat lng ;)
    google map lat lng ;)

--- Gorgias.Web.React/gorgias.react.web/src/Components/Admin/Form/CustomGoogleMap.js
+++ Gorgias.Web.React/gorgias.react.web/src/Components/Admin/Form/CustomGoogleMap.js
@@ -3,7 +3,11 @@
  */
 
 import React, {Component} from "react";
-import Geocode from "react-geocode";
+// import Geocode from "react-geocode";
+
+let DEBUG = false;
+let API_KEY = "AIzaSyAjU94_y64Gh4mCZgDi4Ccdadaw8YRxqek";
+const GOOGLE_API = "https://maps.google.com/maps/api/geocode/json";
 
 export default class CustomGoogleMap extends React.Component {
 
@@ -13,6 +17,43 @@ export default class CustomGoogleMap extends React.Component {
 
     }
 
+    async handleUrl(url) {
+        const response = await fetch(url).catch(error =>
+            Promise.reject(new Error("Error fetching data"))
+        );
+
+        const json = await response.json().catch(() => {
+            console.log("Error parsing server response");
+            return Promise.reject(new Error("Error parsing server response"));
+        });
+
+        if (json.status === "OK") {
+            console.log(json);
+            return json;
+        }
+        console.log(`Server returned status code ${json.status}`, true);
+        return Promise.reject(
+            new Error(`Server returned status code ${json.status}`)
+        );
+    }
+
+
+    async fromAddress(address: string, apiKey: string): Promise {
+        if (!address) {
+            console.log("Provided address is invalid", true);
+            return Promise.reject(new Error("Provided address is invalid"));
+        }
+
+        let url = `${GOOGLE_API}?address=${encodeURI(address)}`;
+
+        if (apiKey || API_KEY) {
+            API_KEY = apiKey || API_KEY;
+            url += `&key=${API_KEY}`;
+        }
+
+        console.log(url);
+        return this.handleUrl(url);
+    }
 
     handleFormSubmit = (submitEvent) => {
         submitEvent.preventDefault();
@@ -20,27 +61,27 @@ export default class CustomGoogleMap extends React.Component {
     }
 
     componentDidMount() {
-        // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
-        Geocode.setApiKey("AIzaSyCfhlnaCgbGf9GlcYpKdi_q-b-hoziWD_Y");
-
-// Enable or disable logs. Its optional.
-        Geocode.enableDebug();
+//         // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+//         Geocode.setApiKey("AIzaSyCfhlnaCgbGf9GlcYpKdi_q-b-hoziWD_Y");
+//
+// // Enable or disable logs. Its optional.
+//         Geocode.enableDebug();
 // Get address from latidude & longitude.
-        Geocode.fromLatLng("48.8583701", "2.2922926").then(
-            response => {
-                const address = response.results[0].formatted_address;
-                console.log(address);
-            },
-            error => {
-                console.error(error);
-            }
-        );
+//         Geocode.fromLatLng("48.8583701", "2.2922926").then(
+//             response => {
+//                 const address = response.results[0].formatted_address;
+//                 console.log(address);
+//             },
+//             error => {
+//                 console.error(error);
+//             }
+//         );
 
 // Get latidude & longitude from address.
-        Geocode.fromAddress("Eiffel Tower").then(
+        this.fromAddress("A15,09, the heritage, seri kembangan, selangor, malaysia, 43300",API_KEY).then(
             response => {
                 const { lat, lng } = response.results[0].geometry.location;
-                console.log(lat, lng);
+                console.log('address la',lat, lng);
             },
             error => {
                 console.error(error);
