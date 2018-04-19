6e954e65f95df977c0d1c5376450ab8cd9015ddc
hseniht <thinesh_kumar_mu@hotmail.com>
2018-04-19 15:18:05 +0800

    Hamburger - edited profile img icon

--- Gorgias.Web.React/gorgias.react.web/src/Components/PageElements/AdminBurgerHeader.js
+++ Gorgias.Web.React/gorgias.react.web/src/Components/PageElements/AdminBurgerHeader.js
@@ -42,39 +42,47 @@ export default class AdminBurgerHeader extends React.Component {
     prepareMenu = () => {
         return (
             <Menu
-              width={ 120 }
+              width={ 100 }
               isOpen={true}
               customCrossIcon={false}
                styles={styles}>
-                <img onClick={this.prepareAccountProfilesModal}
-                     src={this.props.profileAccountSetting.ProfileImage}
-                     style={{
-                         width: "100%",
-                         /*height: 75,*/
-                         display: '',
-                         borderBottomRightRadius: 15,
-                         borderTopLeftRadius: 15, borderTopRightRadius: 15,
-                         borderBottomLeftRadius: 15,
-                     }}/>
-                <a id="home" className="menu-item font-hamburger-nav" href="/">
-                     <span><i className="icon-left-circled icon-larger "></i></span>Home</a>
+                <div className="dashboard-image-container">
+                  <img className="dashboard-image" onClick={this.prepareAccountProfilesModal}
+                       src={this.props.profileAccountSetting.ProfileImage}
+                       style={{
+                           width: "100%",
+                           /*height: 75,*/
+                           display: '',
+                           borderBottomRightRadius: 25,
+                           borderTopLeftRadius: 25, borderTopRightRadius: 25,
+                           borderBottomLeftRadius: 25,
+                       }}/>
+                    <div className="middle-overlay" onClick={this.prepareAccountProfilesModal}>
+                      <div className="text icon-pencil"></div>
+                    </div>
+                  </div>
+
                 <a id="Dashboard" className="menu-item font-hamburger-nav" href="/admin/">
-                  <span><i className="icon-desktop-line icon-larger "></i></span>Dashboard</a>
+                  <span><i className="icon-desktop-line icon-larger "></i><br></br></span>Dashboard</a>
                 <a id="profile" className="menu-item font-hamburger-nav" href="/admin/profile">
-                  <span><i className="icon-user icon-larger "></i></span>Profile</a>
+                  <span><i className="icon-user icon-larger "></i><br></br></span>Profile</a>
                 <a id="story" className="menu-item font-hamburger-nav" href="/admin/story">
-                  <span><i className="icon-docs icon-larger "></i></span>Story</a>
+                  <span><i className="icon-docs icon-larger "></i><br></br></span>Story</a>
                 <a id="contact" className="menu-item font-hamburger-nav" href="/admin/contact">
-                  <span><i className="icon-call icon-larger "></i></span>Contact</a>
+                  <span><i className="icon-call icon-larger "></i><br></br></span>Contact</a>
                 <a id="follower" className="menu-item font-hamburger-nav" href="/admin/follower">
-                  <span><i className="icon-users icon-larger "></i></span>Followers</a>
+                  <span><i className="icon-users icon-larger "></i><br></br></span>Followers</a>
                 <a id="contentmanager" className="menu-item font-hamburger-nav" href="/admin/content/manager">
-                  <span><i className="icon-user-add icon-larger "></i></span>Content Managers</a>
+                  <span><i className="icon-user-add icon-larger "></i><br></br></span>Content<br></br>Managers</a>
 
-                <br></br>
 
-                <a id="contact" className="menu-item font-hamburger-nav" onClick={this.props.logout}>
-                  <span><i className="icon-back"></i></span>logout</a>
+                  <br></br>
+
+                <a id="home" className="menu-item font-hamburger-nav" href="/">
+                  <span><i className="icon-home icon-larger "></i><br></br></span>Home</a>
+
+                {/*<a id="contact" className="menu-item font-hamburger-nav" onClick={this.props.logout}>
+                  <span><i className="icon-back"></i><br></br></span>logout</a>*/}
             </Menu>
         );
     };
@@ -130,9 +138,12 @@ var styles = {
         background: '#ffffff'
     },
     bmMenu: {
-        backgroundColor: 'rgba(0,0,0,0.8)',
+        //backgroundColor: 'rgba(0,0,0,0.8)',
+        backgroundColor: '#222222',
         // background: 'rgba(0,0,0,0.8) url(https://gorgiasasia.blob.core.windows.net/images/profile-1011.jpg) no-repeat center top',
-        padding: '2.5em 1.5em 0',
+
+        //padding: '2.5em 1.5em 0',
+        padding: '50px 15px 5px',
         fontSize: '1.15em',
         lineHeight: '32px',
         textAlign: 'center',
