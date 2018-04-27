0f0d5d4c79afecdc3f6bb29e49ebaab5986479c9
hseniht <thinesh_kumar_mu@hotmail.com>
2018-04-27 14:49:02 +0800

    - removed charts testing widget (commented out)

--- Gorgias.Web.React/gorgias.react.web/src/Components/Admin/Dashboard/DashboardComponent.js
+++ Gorgias.Web.React/gorgias.react.web/src/Components/Admin/Dashboard/DashboardComponent.js
@@ -593,35 +593,39 @@ class DashboardComponent extends Component {
                                             </div>
                                         </div>
 
-                                        {/*test widget*/}
-                                        <div className="column mcb-column one-fourth column_column"
-                                             style={{margin: 1 + "% " + 1 + "%"}}>
-                                            <div className="column_attr align_center card-block-rounded widget-chart">
-                                                  <div className="stats-container">
-                                                    <div className="stats-column left" style={{textAlign:"left"}}>
-                                                        <h6 className="font-db-theme-3 bold">Deposit</h6>
-                                                    </div>
-                                                    <div className="stats-column right" style={{textAlign:"right"}}>
-                                                        <p className="tkFont-Bold highlights">March <span className="fontStats-value">49%</span></p>
-                                                        <p>April <span className="fontStats-value">51%</span></p>
-                                                    </div>
+                                        {/*test widget with chart
 
-                                                    <div class="chart_box">
-                                                        <div class="chart" data-color="#2991d6" data-percent="42">
-                                                            <div class="num"> 42% </div>
-                                                                <canvas height="140" width="140"></canvas>
-                                                        </div>
+                                          <div className="column mcb-column one-fourth column_column"
+                                               style={{margin: 1 + "% " + 1 + "%"}}>
+                                              <div className="column_attr align_center card-block-rounded widget-chart">
+                                                    <div className="stats-container">
+                                                      <div className="stats-column left" style={{textAlign:"left"}}>
+                                                          <h6 className="font-db-theme-3 bold">Deposit</h6>
+                                                      </div>
+                                                      <div className="stats-column right" style={{textAlign:"right"}}>
+                                                          <p className="tkFont-Bold highlights">March <span className="fontStats-value">49%</span></p>
+                                                          <p>April <span className="fontStats-value">51%</span></p>
+                                                      </div>
 
-                                                        <p> Curabitur ipsum </p>
-                                                    </div>
+                                                      <div class="chart_box">
+                                                          <div class="chart" data-color="#2991d6" data-percent="42">
+                                                              <div class="num"> 42% </div>
+                                                                  <canvas height="140" width="140"></canvas>
+                                                          </div>
 
-                                                  <div className="column mcb-column one column_column stats-column" style={{textAlign:"left"}}>
-                                                      <h6 className="tkFont-Bold tkFont-Theme"><span>&#36;</span> 30,000</h6>
-                                                      <p>{hotSpots.TotalFeel}</p>
-                                                  </div>
+                                                          <p> Curabitur ipsum </p>
+                                                      </div>
+
+                                                    <div className="column mcb-column one column_column stats-column" style={{textAlign:"left"}}>
+                                                        <h6 className="tkFont-Bold tkFont-Theme"><span>&#36;</span> 30,000</h6>
+                                                        <p>{hotSpots.TotalFeel}</p>
                                                     </div>
-                                            </div>
-                                        </div>
+                                                      </div>
+                                              </div>
+                                          </div>
+
+                                          */}
+
 
                                         {/*end of new test cards*/}
 
